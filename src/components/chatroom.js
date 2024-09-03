0906850ee921c3497c5301c884001bddc7121e2a
Tushar Jaiswal <Tushar.Jaiswal@pens.com>
2024-09-04 00:53:35 +0530

    testing language options

--- src/components/chatroom.js
+++ src/components/chatroom.js
@@ -32,6 +32,7 @@ const Chatroom = (props) => {
     const [newMessage, setNewMessage] = useState("");
     const [languageTranslate] = useGlobalState('languageTranslate');
     const [languageOptions] = useGlobalState('languageOptions');
+    console.log("languageOptions", languageOptions);
     const agentUsername = 'AGENT';
     const messageEl = useRef(null);
     const input = useRef(null);
@@ -127,7 +128,7 @@ const Chatroom = (props) => {
 
     return (
         <div className="chatroom">
-            {/* < LanguageDropdown setToLanguage={props.setToLanguage} languageOptions={languageOptions} /> */}
+            < LanguageDropdown setToLanguage={props.setToLanguage} languageOptions={languageOptions} />
             <h3>Translate - ({languageTranslate.map(lang => {if(lang.contactId === currentContactId[0])return lang.lang})}) {getKeyByValue(languageOptions)}</h3>
             <ul className="chats" ref={messageEl}>
             {
